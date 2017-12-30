e3540e9caf20431c6dcde538483eb9ae94e15260
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-30 14:10:21 +0100

    estructura de fichero

--- script_control_backups.sh
+++ script_control_backups.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
-grafana1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="grafana" group by Name;')
-grafana_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula.Job where RealEndTime not in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="grafana" group by Name;')
-grafana2=$( echo $grafana1 | cut -d " " -f 7)
-grafana3=$( echo $grafana_fecha | cut -d " " -f 2)
-grafana_estado=$( echo $grafana1 | cut -d " " -f 6)
-echo $grafana2
-echo $grafana3
-echo $grafana_estado
-
-#if [ $grafana2 != $grafana3 ]; then
-#    echo "true"
-#else
-#    echo "false"
-#fi
-
 mickey1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="mickey" group by Name;')
 mickey_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula.Job where RealEndTime not in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="mickey" group by Name;')
 mickey2=$( echo $mickey1 | cut -d " " -f 7)
 mickey3=$( echo $mickey_fecha | cut -d " " -f 2)
 mickey_estado=$( echo $mickey1 | cut -d " " -f 6)
+mickey_tipo=$( echo $mickey1 | cut -d " " -f 5)
+mickey_tamano=$( echo $mickey1 | cut -d " " -f 9)
 echo $mickey2
 echo $mickey3
 echo $mickey_estado
+echo $mickey_tipo
+echo $mickey_tamano
 
 #if [ $mickey2 != $mickey3 ]; then
-#    echo "true"
+#  echo "inset into backups values ('juanjose.lopez', '172.22.200.78','' )"
 #else
 #    echo "false"
 #fi
@@ -34,10 +23,13 @@ minnie_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula
 minnie2=$( echo $minnie1 | cut -d " " -f 7)
 minnie3=$( echo $minnie_fecha | cut -d " " -f 2)
 minnie_estado=$( echo $minnie1 | cut -d " " -f 6)
+minnie_tipo=$( echo $mickey1 | cut -d " " -f 5)
+minnie_tamano=$( echo $mickey1 | cut -d " " -f 9)
 echo $minnie2
 echo $minnie3
 echo $minnie_estado
-
+echo $minnie_tipo
+echo $minnie_tamano
 #if [ $minnie2 != $minnie3 ]; then
 #    if
 #else
@@ -50,9 +42,13 @@ donald_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula
 donald2=$( echo $donald1 | cut -d " " -f 7)
 donald3=$( echo $donald_fecha | cut -d " " -f 2)
 donald_estado=$( echo $donald1 | cut -d " " -f 6)
+donald_tipo=$( echo $mickey1 | cut -d " " -f 5)
+donald_tamano=$( echo $mickey1 | cut -d " " -f 9)
 echo $donald2
 echo $donald3
 echo $donald_estado
+echo $donald_tipo
+echo $donald_tamano
 
 #if [ $donald2 != $donald3 ]; then
 #    echo "true"
