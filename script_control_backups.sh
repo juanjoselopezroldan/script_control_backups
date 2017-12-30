f54339339957f892f7ac4cacb68a4ffa7e82a8dc
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-30 17:33:27 +0100

    estructura de fichero

--- script_control_backups.sh
+++ script_control_backups.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mickey1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="mickey" group by Name;')
-mickey_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula.Job where RealEndTime not in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="mickey" group by Name;')
 mickey2=$( echo $mickey1 | cut -d " " -f 7 | cut -d "-" -f 3)
 mickey3=$( date | cut -d " " -f 3)
 mickey_estado=$( echo $mickey1 | cut -d " " -f 6)
@@ -12,14 +11,13 @@ echo $mickey_estado
 echo $mickey_tipo
 echo $mickey_tamano
 
-if [ $mickey2 != $mickey3 ]; then
+if [ $mickey2 == $mickey3 ]; then
   echo "inset into backups (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('juanjose.lopez', '172.22.200.78','$mickey_tipo', '$mickey2 con tamano de $mickey_tamano', '$mickey_estado', 'Automatica')"
 else
-  echo "false"
+  echo "inset into backups (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('juanjose.lopez', '172.22.200.78','null', 'No se ha realizado la copia, por favor realice una comprobacion', 'F', 'Erronea')"
 fi
 
 minnie1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="minnie" group by Name;')
-minnie_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula.Job where RealEndTime not in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="minnie" group by Name;')
 minnie2=$( echo $minnie1 | cut -d " " -f 7 | cut -d "-" -f 3)
 minnie3=$( date | cut -d " " -f 3)
 minnie_estado=$( echo $minnie1 | cut -d " " -f 6)
@@ -30,15 +28,15 @@ echo $minnie3
 echo $minnie_estado
 echo $minnie_tipo
 echo $minnie_tamano
-#if [ $minnie2 != $minnie3 ]; then
-#    if
-#else
-#    echo "false"
-#fi
+if [ $minnie2 == $minnie3 ]; then
+echo "inset into backups (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('juanjose.lopez', '172.22.200.78','$mickey_tipo', '$mickey2 con tamano de $mickey_tamano', '$mickey_estado', 'Automatica')"
+else
+echo "inset into backups (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('juanjose.lopez', '172.22.200.78','null', 'No se ha realizado la copia, por favor realice una comprobacion', 'F', 'Erronea')"
+fi
+
 
 
 donald1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="donald" group by Name;')
-donald_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula.Job where RealEndTime not in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="donald" group by Name;')
 donald2=$( echo $donald1 | cut -d " " -f 7 | cut -d "-" -f 3)
 donald3=$( date | cut -d " " -f 3)
 donald_estado=$( echo $donald1 | cut -d " " -f 6)
@@ -51,9 +49,10 @@ echo $donald_tipo
 echo $donald_tamano
 
 #if [ $donald2 != $donald3 ]; then
-#    echo "true"
-#else
-#    echo "false"
-#fi
+echo "inset into backups (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('juanjose.lopez', '172.22.200.78','$mickey_tipo', '$mickey2 con tamano de $mickey_tamano', '$mickey_estado', 'Automatica')"
+else
+echo "inset into backups (backup_user, backup_host, backup_label, backup_description, backup_status, backup_mode) values ('juanjose.lopez', '172.22.200.78','null', 'No se ha realizado la copia, por favor realice una comprobacion', 'F', 'Erronea')"
+fi
+
 
 #conexion con postgresql : psql -h 172.22.200.110  -U juanjose.lopez -d db_backup -c 'select * from backups;'
