65228e244144097dc455b8498e314b19afc9fbba
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-30 12:45:10 +0100

    prueba de comandos

--- script_control_backups.sh
+++ script_control_backups.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 grafana1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="grafana" group by Name;')
 grafana_fecha=$( mariadb -u root -p'root' -e 'select max(RealEndTime) from bacula.Job where RealEndTime not in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="grafana" group by Name;')
-grafana2=$( echo $grafana1 | cut -d " " -f 9)
-grafana3=$( echo $grafana_fecha | cut -d " " -f 1)
-echo $grafana2
+grafana2=$( echo $grafana1 | cut -d " " -f 7)
+grafana3=$( echo $grafana_fecha | cut -d " " -f 2)
+
 
 mickey1=$( mariadb -u root -p'root' -e 'select Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="mickey" group by Name;')
 mickey3=$( echo $mickey1 | cut -d " " -f 6-11)
