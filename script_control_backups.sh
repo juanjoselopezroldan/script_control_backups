3a842477446b29f06f6874eafa909cdc2dd17370
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-30 12:29:53 +0100

    prueba de comandos

--- script_control_backups.sh
+++ script_control_backups.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 grafana1=$( mariadb -u root -p'root' -e 'select Name, Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="grafana" group by Name;')
-grafana2=$( echo $grafana | cut -d " " -f 6-11)
+grafana2=$( echo $grafana1 | cut -d " " -f 6-11)
 
 mickey1=$( mariadb -u root -p'root' -e 'select Name, Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="mickey" group by Name;')
-mickey3=$( echo $mickey | cut -d " " -f 6-11)
+mickey3=$( echo $mickey1 | cut -d " " -f 6-11)
 
 minnie1=$( mariadb -u root -p'root' -e 'select Name, Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="minnie" group by Name;')
-minnie3=$( echo $minnie | cut -d " " -f 6-11)
+minnie3=$( echo $minnie1 | cut -d " " -f 6-11)
 
 donald1=$( mariadb -u root -p'root' -e 'select Name, Level, JobStatus, RealEndTime, (JobBytes/1024)/1024 from bacula.Job where RealEndTime in (select max(RealEndTime) from bacula.Job group by Name) and type="B" and Name="donald" group by Name;')
-donald3=$( echo $donald | cut -d " " -f 6-11)
+donald3=$( echo $donald1 | cut -d " " -f 6-11)
 
 #conexion con postgresql : psql -h 172.22.200.110  -U juanjose.lopez -W -d db_backup -c 'select * from backups;'
